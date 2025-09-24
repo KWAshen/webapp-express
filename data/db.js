7f0ec6a3b7449bc18aa73e643cf62fdf55e75039
KWAshen <ashen.maliaha@gmail.com>
2025-09-24 18:15:02 +0200

    connesso al database

---
+++ data/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '00000000',
+  database: 'movies_db'
+});
+connection.connect((err) => {
+if (err) throw err;
+console.log('Connected to MySQL!');
+});
+module.exports = connection;
