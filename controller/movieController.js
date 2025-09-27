024804f8e0c0201521343f91f7036bc96fb592b0
KWAshen <ashen.maliaha@gmail.com>
2025-09-27 17:36:38 +0200

    index

--- controller/movieController.js
+++ controller/movieController.js
@@ -1,2 +1,20 @@
 const connection=require("../data/db")
 
+const index=(req,res)=>{
+    const sql = "SELECT * FROM movies";
+
+    connection.query(sql,(err,results)=>{
+        if(err){
+            return res.status(500).json({error: `Errore nella query: ${err}`})
+        }
+
+        const movies = results.map(movie => {
+            return {
+                ...movie,
+                image: req.imagePath + movie.image
+            }
+        })
+
+        res.send(movies);
+    })
+}
