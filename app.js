835f0224fe75dadf331308fcaef58ae58dd32579
KWAshen <ashen.maliaha@gmail.com>
2025-09-29 12:13:38 +0200

    cors-env su package.json

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require ("cors")
+const imagePathMiddleware = require ("./middlewares/imagePathMiddleware")
 const list = {
 ////////
 }
@@ -8,6 +10,9 @@ app.get('/', (req, res) => {
 res.send('Rotta base')
 })
 app.use("/api/movie",movieRouter)
+app.use(cors({origin:process.env.FE_APP}))
+app.use(imagePathMiddleware)
+app.use(express.json())
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
